b1616703f3f903564d5e79d1cf88691188515c22
S Varshith <56431994+varshith03@users.noreply.github.com>
2024-04-05 15:50:08 +0530

    created skill sets ui

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,35 +5,39 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 const items = [
   {
     id: 1,
-    title: "Weather App",
-    img: "/project_weather.png",
-    desc: "The Weather App allows users to check the current weather conditions for a specific city or obtain weather information based on their device's location.",
-    code: "https://github.com/varshith03/weather-app-react",
-    deployment: "https://miweatherapp.netlify.app/",
+    title: "Chatty",
+    img: "/project_chatty.png",
+    desc: "Chatty is a feature-rich chat application that offers real-time messaging, responsive design, and support for various media types, providing users with a seamless and engaging communication experience.",
+    code: "https://github.com/varshith03/Chatty-frontend",
+    deployment: "https://chatty-webapp.vercel.app/",
+    skills: ["ReactJS", "NodeJS", "ExpressJS", "MongoDB", "Socket.IO"],
   },
   {
     id: 2,
-    title: "White board App",
-    img: "/project_whiteboard.png",
-    desc: "Welcome to the Real-Time Whiteboard Sharing App! This application allows users to collaborate seamlessly on a virtual whiteboard, fostering real-time communication and creativity.",
-    code: "https://github.com/varshith03/Real-Time-WhiteBoard-Sharing-App",
-    deployment: "https://whiteboard-project.vercel.app/",
-  },
-  {
-    id: 3,
     title: "BookMyDoctor",
     img: "/project_doctor.png",
     desc: "Created the BookMyDoctor platform to modernize healthcare appointment management. The system provides efficient scheduling, reduces wait times, and enhances patient satisfaction.",
     code: "https://github.com/varshith03/BookMyDoctor",
     deployment: "https://svaars.netlify.app/",
+    skills: ["ReactJS", "NodeJS", "ExpressJS", "MongoDB"],
+  },
+  {
+    id: 3,
+    title: "Weather App",
+    img: "/project_weather.png",
+    desc: "The Weather App allows users to check the current weather conditions for a specific city or obtain weather information based on their device's location.",
+    code: "https://github.com/varshith03/weather-app-react",
+    deployment: "https://miweatherapp.netlify.app/",
+    skills: ["ReactJS"],
   },
   {
     id: 4,
-    title: "Chatty(in development)",
-    img: "/project_chatty.png",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    code: "https://github.com/varshith03/Chatty-frontend",
-    deployment: "",
+    title: "White board App",
+    img: "/project_whiteboard.png",
+    desc: "Welcome to the Real-Time Whiteboard Sharing App! This application allows users to collaborate seamlessly on a virtual whiteboard, fostering real-time communication and creativity.",
+    code: "https://github.com/varshith03/Real-Time-WhiteBoard-Sharing-App",
+    deployment: "https://whiteboard-project.vercel.app/",
+    skills: ["ReactJS", "NodeJS", "ExpressJS", "Socket.IO"],
   },
 ];
 
@@ -60,6 +64,13 @@ const Single = ({ item }) => {
         <motion.div className="text-container" style={{ y }}>
           <h2>{item.title}</h2>
           <p>{item.desc}</p>
+          <div className="badge-container">
+            {item.skills.map((skill, index) => (
+              <div className="badge" key={index}>
+                {skill}
+              </div>
+            ))}
+          </div>
           <div className="button-container">
             <motion.button
               whileHover={{ scale: 1.1 }}
